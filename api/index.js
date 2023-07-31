32dad5d6c6a5df62fb859e538cf2e58d53029785
apfomeMatt <matt@apfome.com>
2023-07-31 22:54:58 +0100

    Update to api/index.js

--- api/index.js
+++ api/index.js
@@ -2,35 +2,32 @@ const express = require('express');
 const promMid = require('express-prometheus-middleware');
 
 const PORT = process.env.PORT || 3001;
-
 const createApp = () => {
   const app = express();
 
   const verifyAuthorization = (req, res, next) => {
     const authHeader = req.headers.authorization;
+    console.log(authHeader);
     if (!authHeader || authHeader !== 'mysecrettoken') {
       return res.status(403).json({ error: 'Forbidden' });
     }
     next();
   };
 
-  const metricsMiddleware = promMid({
-    metricsPath: '/metrics',
-    collectDefaultMetrics: true,
-  });
+  app.use('/metrics', verifyAuthorization);
 
-  const timeRouteHandler = (req, res) => {
-    res.json({ epoch: Math.floor(Date.now() / 1000) });
-  };
-
-  const timeMiddleware = (req, res, next) => {
-    verifyAuthorization(req, res, next);
-  };
+  app.use(
+    promMid({
+      metricsPath: '/metrics',
+      collectDefaultMetrics: true,
+    })
+  );
 
   app.use(verifyAuthorization);
 
-  app.get('/time', timeMiddleware, timeRouteHandler);
-  app.use('/metrics', metricsMiddleware);
+  app.get('/time', (req, res) => {
+    res.json({ epoch: Math.floor(Date.now() / 1000) });
+  });
 
   return app;
 };
